41ca33a49b1126b93a2b351b4459931b16289f17
Saafin <72722412+Saafin@users.noreply.github.com>
2020-11-09 10:58:27 +0600

    Add files via upload

---
+++ LAB6/LAB6/Students.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB6
+{
+    class Students
+    {
+        private
+        String name;
+        String id;
+        String department;
+        float cgpa;
+        private Students()
+        { }
+        public Students(String nm, String idd, String dep, float cg)
+        {
+            name = nm;
+            id = idd;
+            department = dep;
+            cgpa = cg;
+        }
+        public void ShowInfo()
+        {
+            Console.WriteLine("Name of this Student is: " + name);
+            Console.WriteLine("ID of this Student is: " + id);
+            Console.WriteLine("Department of this Student is: " + department);
+            Console.WriteLine("CGPA of this Student is: " + cgpa);
+        }
+
+    }
+}
---
+++ LAB6/LAB6/Studentsm.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB6
+{
+    class Studentsm
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Write your name:");
+            String n = Console.ReadLine();
+            Console.WriteLine("Write your ID:");
+            String i = Console.ReadLine();
+            Console.WriteLine("Write your Department:");
+            String d = Console.ReadLine();
+            Console.WriteLine("Write your CGPA:");
+            float c = Console.Read();
+            Students s = new Students(n, i, d, c);
+            s.ShowInfo();
+        }
+    }
+}
+
