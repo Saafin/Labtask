aadca7358a294ab08b8036b157dbab1c94ec93f3
Saafin <72722412+Saafin@users.noreply.github.com>
2020-11-29 02:35:27 +0600

    Interface 3
    Interface 3

---
+++ Radio-player-Interface 3/Ipod.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Radioplayer
+{
+    class Ipod : IMusicPlayer
+    {
+        public void Play(bool on)
+        {
+            Console.WriteLine("Music playing");
+        }
+
+        public void PlayNext()
+        {
+            Console.WriteLine("Playing next track");
+        }
+
+        public void PlayPrevious()
+        {
+            Console.WriteLine("Playing previous track");
+        }
+
+        public void SetVolume(int loudness)
+        {
+            Console.WriteLine("Volume set to " + loudness);
+        }
+
+        public void Switch(bool on)
+        {
+            Console.WriteLine("Turning switch ON");
+        }
+    }
+}
